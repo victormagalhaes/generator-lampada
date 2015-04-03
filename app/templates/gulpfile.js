77574de9e594450d88ce17b5a38a91c9efd9f891
Victor Magalhães <aheia1412@gmail.com>
2015-04-03 19:52:13 -0300

    Ajustando o gulpfile e os parametros

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -9,7 +9,7 @@ var runSequence = require('run-sequence');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
 var jade = require('gulp-jade');
@@ -17,86 +17,112 @@ var jade = require('gulp-jade');
 
 // Coffee
 gulp.task('coffee', function() {
+    // runSequence('limpar-js');
     gulp
         .src(parametros.scripts + '/*.coffee')
         .pipe(plumber())
         .pipe(coffee({ bare: true}))
         .pipe(plumber.stop())
         .pipe(gulp.dest(parametros.destino + '/js'))
-        .on('error', function () { gutil.log(); });
+        .on('error', function() { gutil.log(); });
 });
 
 
 // Minificar JS
 gulp.task('minificar-js', function() {
+    // runSequence('limpar-js');
     gulp
         .src(parametros.destino + '/js/**.js')
         .pipe(plumber())
         .pipe(uglify({ outSourceMap: true}))
         .pipe(plumber.stop())
         .pipe(gulp.dest(parametros.destino + '/js'))
-        .on('error', function () { gutil.log(); });
+        .on('error', function() { gutil.log(); });
 });
 
 
 // Sass
-gulp.task('sass', function () {
+gulp.task('sass', function() {
+    // runSequence('limpar-css');
     gulp
-        .src(parametros.arquivo_base_sass)
+        .src(parametros.estilos + '/*.scss')
         .pipe(plumber())
-        .pipe(sass({ sourcemap: false, sourcemapPath: '.' }))
+        .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(plumber.stop())
         .pipe(gulp.dest(parametros.destino + '/css'))
-        .on('error', function () { gutil.log(); });
+        .on('error', function() { gutil.log(); });
 });
 
 
 // Minificar CSS
 gulp.task('minificar-css', function() {
-  gulp.src(parametros.destino + '/css/*.css')
-    .pipe(plumber())
-    .pipe(minifyCSS())
-    .pipe(plumber.stop())
-    .pipe(gulp.dest(parametros.destino + '/css'))
-    .on('error', function () { gutil.log(); });
+    // runSequence('limpar-css');
+    gulp.src(parametros.destino + '/css/*.css')
+        .pipe(plumber())
+        .pipe(minifyCSS())
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(parametros.destino + '/css'))
+        .on('error', function() { gutil.log(); });
 });
 
 
 // Jade
 gulp.task('jade', function() {
+    // runSequence('limpar-html');
     gulp
         .src(parametros.templates + '/*.jade')
         .pipe(plumber())
         .pipe(jade({ pretty: true}))
         .pipe(plumber())
         .pipe(gulp.dest(parametros.destino))
-        .on('error', function () { gutil.log(); });
+        .on('error', function() { gutil.log(); });
 });
 
 
 // Imagens
 gulp.task('imagens', function() {
+    // runSequence('limpar-imagens');
     gulp
-        .src(parametros.recursos + '/imagens/**')
+        .src(parametros.imagens + '/*')
         .pipe(gulp.dest(parametros.destino + '/imagens'))
-        .on('error', function () { gutil.log(); });
+        .on('error', function() { gutil.log(); });
 });
 
 
 // Monitorar, ou seja: observar os arquivos
 gulp.task('monitorar', function() {
-    runSequence('compilar-origem');
-    gulp.watch(parametros.recursos + '/**/*.coffee', ['coffee']);
-    gulp.watch(parametros.recursos + '/**/*.sass', ['sass']);
-    gulp.watch(parametros.recursos + '/**/*.jade', ['jade']);
-    gulp.watch(parametros.recursos + '/*', ['imagens']);
+    runSequence('limpar-css', 'limpar-js', 'limpar-html', 'limpar-imagens', 'compilar-origem');
+    gulp.watch(parametros.scripts + '/*.coffee', ['coffee']);
+    gulp.watch(parametros.estilos + '/*.scss', ['sass']);
+    gulp.watch(parametros.templates + '/*.jade', ['jade']);
+    gulp.watch(parametros.imagens + '/*', ['imagens']);
 
 });
 
+
+// Deletar os arquivos da pasta destino/css
+gulp.task('limpar-css',function() {
+    del([parametros.destino + '/css/*.css']);
+});
+
+// Deletar os arquivos .html da pasta destino
+gulp.task('limpar-html',function() {
+    del([parametros.destino + '*.html']);
+});
+
+// Deletar os arquivos da pasta destino/js
+gulp.task('limpar-js',function() {
+    del([parametros.destino + '/js/*.js']);
+});
+
+// Deletar os arquivos da pasta destino/imagens
+gulp.task('limpar-imagens',function() {
+    del([parametros.destino + '/imagens/*']);
+});
 
 // Compilar origem
 gulp.task('compilar-origem', function() {
--- app/templates/parametros.js
+++ app/templates/parametros.js
@@ -1,4 +1,6 @@
 var aplicacao = './aplicacao';
+var origem = './aplicacao/origem';
+var destino = './aplicacao/destino';
 
 var parametros = {
 
@@ -6,25 +8,25 @@ var parametros = {
     aplicacao: aplicacao,
 
     // Caminho onde ficam todos os arquivos fontes
-    origem: aplicacao + '/origem',
+    origem: origem,
 
     // Caminho para qual serão gerados os arquivos otimizados
-    destino: aplicacao + '/destino',
+    destino: destino,
 
     // Caminho dos templates
-    templates: aplicacao + '/templates',
+    templates: origem + '/templates',
 
     // Caminho dos arquivos de estilos
-    estilos: aplicacao + '/estilos',
+    estilos: origem + '/estilos',
 
     // Caminho dos scripts
-    scripts: aplicacao + '/scripts',
+    scripts: origem + '/scripts',
 
     // Caminho das imagens
-    imagens: aplicacao + '/imagens',
+    imagens: origem + '/imagens',
 
     // Nome do arquivo que faz todos os includes de SASS da framework
-    arquivo_base_sass: aplicacao + '/estilos/base.sass',
+    arquivo_base_sass: origem + '/estilos/base.sass',
 
     // Minificar arquivos na compilação?
     minificar: true
